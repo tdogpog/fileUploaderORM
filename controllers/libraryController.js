6d6a741348fa2ebc5f5aaff6fdb2957d14302ee4
tdog <161662619+tdogpog@users.noreply.github.com>
2024-12-10 17:47:20 -0600

    Added in multer and defined url structure for eventual CRUD processes

--- controllers/libraryController.js
+++ controllers/libraryController.js
@@ -1,6 +1,27 @@
+//this is going to grab a root file in the database that contains EVERYTHING else, and render
+//all of its contents
 function getLibrary(req, res) {
   res.render("library", { title: "Library" });
 }
+
+//renders the contents of the folder when you click on it
+function getFolder(req, res) {}
+
+//creates a new folder in whatever directory you're in
+//this will use the url as the url will update as you're navigating
+function createFolder(req, res) {}
+
+//uploads a file the same way create folder does, using the url to direct it
+function uploadFile(req, res) {}
+
+//deletes a file using the url
+function deleteFile(req, res) {}
+
+//deletes an entire folder recursively using the url
+function deleteMethod(req, res) {}
+
+//renames using the url
+function renameMethod(req, res) {}
 
 module.exports = {
   getLibrary,
--- routes/libraryRouter.js
+++ routes/libraryRouter.js
@@ -2,10 +2,36 @@ const { Router } = require("express");
 
 const { getLibrary } = require("../controllers/libraryController");
 
+const multer = require("multer");
+
+const upload = multer({
+  limits: { fileSize: 10485760 }, // Limit file size to 10MB
+  dest: "uploads/", // Save files in the "uploads" folder
+});
+
 const libraryRouter = Router();
 
 //define the route at the root of the base path defined in app.js
 // this will set to /library
 libraryRouter.get("/", getLibrary);
+
+//renders the contents of the folder
+libraryRouter.get("/:folderID", getFolder);
+
+//MULTER PROCESS
+// Upload a file to the specified folder
+libraryRouter.post(
+  "/:folderId/upload",
+  upload.single("uploaded_file"),
+  uploadFile
+);
+
+libraryRouter.post("/:folderID/add-sub", postSubfolder);
+
+//the database needs to know if its a folder/file so track its type
+//along with its ID for
+//updating and deletion
+libraryRouter.post("/:type/:id/rename", renameMethod);
+libraryRouter.post("/:type/:id/delete", deleteMethod);
 
 module.exports = libraryRouter;
